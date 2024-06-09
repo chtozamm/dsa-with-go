3e9ad01efc22ba41bd971fe27ccf8bbab446f6c3
Mark M <chtozamm@gmail.com>
2024-06-09 10:31:51 +0300

    Add binary search with tests

---
+++ algorithms/binary_search/binary_search.go
@@ -0,0 +1,22 @@
+package main
+
+// BinarySearch searches for needle in a sorted slice of ints and returns its index.
+// Returns -1 if needle is not present in haystack. The slice must be sorted in ascending order.
+func BinarySearch(haystack []int, needle int) int {
+	low := 0
+	high := len(haystack) - 1
+
+	for low <= high {
+		mid := (low + high) / 2
+		if haystack[mid] == needle {
+			return mid
+		}
+		if haystack[mid] < needle {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+
+	return -1
+}
---
+++ algorithms/binary_search/binary_search_test.go
@@ -0,0 +1,52 @@
+package main
+
+import "testing"
+
+func TestBinarySearch(t *testing.T) {
+	cases := []struct {
+		Name     string
+		Haystack []int
+		Needle   int
+		Expected int
+	}{
+		{
+			"positive integers",
+			[]int{1, 2, 3, 4, 5, 6},
+			5,
+			4,
+		},
+		{
+			"with negative integers",
+			[]int{-5, -2, 0, 4, 7},
+			-2,
+			1,
+		},
+		{
+			"returns -1 if value not in slice",
+			[]int{1, 2, 3, 4, 5, 6},
+			7,
+			-1,
+		},
+		{
+			"needle is the first value",
+			[]int{1, 2, 3, 4, 5, 6},
+			1,
+			0,
+		},
+		{
+			"needle is the last value",
+			[]int{1, 2, 3, 4, 5, 6},
+			6,
+			5,
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			got := BinarySearch(test.Haystack, test.Needle)
+			if got != test.Expected {
+				t.Errorf("got %d, expected %d", got, test.Expected)
+			}
+		})
+	}
+}
